e7d9d9ba9d047af9082057eabc74edf336660b07
Emir Balic <emirbalic@gmail.com>
2020-11-27 14:01:50 +0100

    Basic Auth

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; } // capital N for aspidentiy
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
