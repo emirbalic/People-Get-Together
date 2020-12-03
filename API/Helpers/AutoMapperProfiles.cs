066dd2065e8f0b93468f3b256b3c3f1daab8d2c4
Emir Balic <emirbalic@gmail.com>
2020-12-03 13:06:52 +0100

    Extending User, Projections

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(
+                    src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                // == instead of using a method within the entity ==
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
