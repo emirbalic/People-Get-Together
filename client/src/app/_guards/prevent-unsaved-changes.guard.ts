17674ccd38c2fa0d908405c2e82806d245b1e2ae
Emir Balic <emirbalic@gmail.com>
2020-12-10 17:38:38 +0100

    Forms, Interceptors, Client side update

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  CanDeactivate,
+} from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  canDeactivate(component: MemberEditComponent): boolean {
+    if (component.editForm.dirty) {
+      return confirm(
+        'Sure you want to continue? Unsaved changes will be lost!'
+      );
+    }
+    return true;
+  }
+}
