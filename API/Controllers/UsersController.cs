f5186f21c231cf7eac3cd2e702fea50eb4cd44ea
Emir Balic <emirbalic@gmail.com>
2020-12-29 13:13:38 +0100

    Basic Asp.Identity

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -29,6 +29,7 @@ namespace API.Controllers
             _mapper = mapper;
             _userRepository = userRepository;
         }
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
         {
@@ -47,6 +48,7 @@ namespace API.Controllers
 
             return Ok(users);
         }
+
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
--- API/Extensions/IdentityServices.cs
+++ API/Extensions/IdentityServices.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -9,20 +12,38 @@ namespace API.Extensions
     public static class IdentityServices
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-            IConfiguration config)   
+            IConfiguration config)
         {
-              services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options => 
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                    };
-                });
-                return services;
+            services.AddIdentityCore<AppUser> (opt => 
+            {
+                opt.Password.RequireNonAlphanumeric = true;
+            }).AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddRoleValidator<RoleValidator<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>(); 
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                      ValidateIssuer = false,
+                      ValidateAudience = false,
+                  };
+              });
+
+            services.AddAuthorization(opt => 
+            {
+                 opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                 opt.AddPolicy("MOderatePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+            
+            return services;
         }
+
     }
 }
